25381928d936257e8fdff0928fe18aecd6d3d0c8
FazaViranza <nofila.faza.ft24@mail.umy.ac.id>
2025-06-10 18:12:02 +0700

    Membuka file dalam mode menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -2,3 +2,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+    cout << "Masukan Nama Fila : ";
+    cin >> NamaFile;
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+
+}
